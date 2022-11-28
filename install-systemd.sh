77b8004c70fadb5729abe78670e5bb1d776b22da
Travis Hardiman <dieseltravis@users.noreply.github.com>
2022-11-27 21:19:06 -0500

    install for /home/pi/conky, and script updates

--- install-systemd.sh
+++ install-systemd.sh
@@ -3,6 +3,13 @@
 INFO='\e[38;05;14m'
 NC='\033[0m' # No Color
 
+echo "Installing python requirements..."
+echo -e "${INFO}pip install -r requirements.txt${NC}";
+pip install -r requirements.txt
+echo "Setting ownership..."
+echo -e "${INFO}chown pi:pi ~/conky/*.*${NC}";
+sudo chown pi:pi ~/conky/*.*
+echo "Installing systemd service..."
 echo -e "${INFO}cp ~/conky/conky.service /lib/systemd/system/${NC}";
 sudo cp ~/conky/conky.service /lib/systemd/system/
 echo -e "${INFO}systemctl enable conky${NC}";
